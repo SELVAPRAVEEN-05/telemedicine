f70cfa8a19c7ebc2792140cba2af5bb71fd1235d
SELVAPRAVEEN-05 <selvapraveen2005@gmail.com>
2025-09-13 00:30:10 +0530

    dashboard

--- src/route/appNavigator.tsx
+++ src/route/appNavigator.tsx
@@ -2,22 +2,25 @@ import React from "react";
 import Landing from "../screen/landing";
 import Login from "../screen/login";
 import Register from "../screen/register";
+import PatientDashboard from "../screen/patientDashboard";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export type RootStackParamList = {
   Landing: undefined;
   Login: undefined;
   Register: undefined;
+  PatientDashboard: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Landing">
+    <Stack.Navigator initialRouteName="PatientDashboard">
       <Stack.Screen name="Landing" component={Landing} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="PatientDashboard" component={PatientDashboard} />
     </Stack.Navigator>
   );
 }
